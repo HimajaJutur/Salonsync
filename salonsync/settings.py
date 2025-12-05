3d428cde9721f87a94a1c7c6f44e5571717bf295
JuturHimaja <x24238708@student.ncirl.ie>
2025-12-05 21:51:17 +0000

    Blocker issue

--- salonsync/settings.py
+++ salonsync/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d_gm=0*bzmh-@+1v$$w)+5vtz$%p*snkw1az)(=%_i$^%xcn*&"
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'himajajutur@gmail.com'        # 👈 replace with your Gmail
-EMAIL_HOST_PASSWORD = 'gtdppwocixxxbgxo'      # 👈 create App Password (see below)
+EMAIL_HOST_USER = 'himajajutur@gmail.com'        
+EMAIL_HOST_PASSWORD = 'gtdppwocixxxbgxo'     
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MEDIA_URL = '/media/'
